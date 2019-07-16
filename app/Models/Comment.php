2acdad457644b8f97c73ab5dbb104e7a46b60627
veljko-d <veljko.d@protonmail.com>
2019-07-16 12:00:53 +0200

    Some minor modifications

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -12,6 +12,15 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'content',
+    ];
+
+    /**
      * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
      */
     public function post()
--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -12,6 +12,15 @@ use Illuminate\Database\Eloquent\Model;
 class Tag extends Model
 {
     /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+    ];
+
+    /**
      * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
      */
     public function posts()
